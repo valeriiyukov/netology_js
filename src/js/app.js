15086ce4afabb47fa0f262adc48932a5f406da93
yukov <valeryyukov@gmail.com>
2019-01-29 19:51:32 +0500

    Правки по коду

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,19 @@
 'use strict';
-let priceList = [200, 550, 4000, 23, 58, 5000, 485, 711];
-let bonus = 5;
-let minPrice = 10000;
+const priceList = [200, 550, 4000, 23, 58, 5000, 485, 711];
+const bonus = 5;
+const minPrice = 10000;
 
-function calculateBonus(price, bonus, minPrice, summ = 0) {
+const calculateBonus = (price, bonus, minPrice, summ = 0) => {
     for (let value of price) {
         summ += value;
     }
-    
+    console.log(summ)
     if (summ >= minPrice) {
-        let totalBonus = Math.floor(summ / 100 * bonus);
+        let totalBonus = Math.floor((summ - minPrice) / 100 * bonus);
         return totalBonus;
     } else {
         return 'Бонус начисляется только при покупке свыше 10 000 игровых баллов'
     }
 }
 
-document.getElementById("bonus").innerHTML = calculateBonus(priceList, bonus, minPrice);
+console.log(calculateBonus(priceList, bonus, minPrice));
